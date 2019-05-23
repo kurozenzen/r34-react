f23495f660c0a7018bbe4b80f9ec3af9179b6130
kurozenzen <kurozenzen@gmail.com>
2019-05-23 03:34:58 +0200

    improved highlighting

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -9,15 +9,15 @@ export default class Tag extends Component {
     this.state = {
       name: props.name,
       count: props.count,
-      onClick: props.onClick,
       modifier: props.modifier
     }
-  }
 
+    this.onClick = props.onClick
+  }
 
   render() {
     return (
-      <span className={"badge badge-tag" + (activeTags.has(this.state.name) ? ' active' : ' ') + (this.state.modifier === "-" ? ' exclude' : ' ')} onClick={this.state.onClick}>
+      <span className={"badge badge-tag" + (activeTags.has(this.state.name) ? ' active' : ' ') + (this.state.modifier === "-" ? ' exclude' : ' ')} onClick={this.onClick} onTouchEnd={this.releaseFocus}>
         {this.state.count ? this.state.name +" (" + this.state.count + ")" : this.state.name}
       </span>
     )
@@ -28,7 +28,7 @@ export function TagList(props) {
   return (    
     <div className="tag-list">
       {props.tags.map(tag =>
-        <Tag key={"t_"+ tag.name} name={tag.name} count={tag.posts} modifier={tag.modifier} onClick={() => props.onClick(tag)}/>
+        <Tag key={"t_"+ tag.name} name={tag.name} count={tag.posts} modifier={tag.modifier} onClick={() => props.onClick(tag)} />
       )}
     </div>
   );

4ea244f8368da1b1b1a69ac762540aed532154f7
kurozenzen <kurozenzen@gmail.com>
2019-08-20 23:23:17 +0100

    Updates

--- precache-manifest.5a2b1f8d070796efdaa3793166523dcc.js
+++ precache-manifest.5a2b1f8d070796efdaa3793166523dcc.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/r34-react/static/js/runtime~main.25a9fe58.js"
   },
   {
-    "revision": "4a61adc736bd7fefe9d5",
-    "url": "/r34-react/static/js/main.4a61adc7.chunk.js"
+    "revision": "afc6ef65fd3b891edda1",
+    "url": "/r34-react/static/js/main.afc6ef65.chunk.js"
   },
   {
     "revision": "daa0c91db6df5a73082e",
     "url": "/r34-react/static/js/1.daa0c91d.chunk.js"
   },
   {
-    "revision": "4a61adc736bd7fefe9d5",
-    "url": "/r34-react/static/css/main.6f505808.chunk.css"
+    "revision": "afc6ef65fd3b891edda1",
+    "url": "/r34-react/static/css/main.c0f8194d.chunk.css"
   },
   {
     "revision": "daa0c91db6df5a73082e",
     "url": "/r34-react/static/css/1.e948b4c7.chunk.css"
   },
   {
-    "revision": "b7610845e3f66fcbf724029df9c5cf48",
+    "revision": "ab2664a8a658be2c70deac690e2b2783",
     "url": "/r34-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/r34-react/precache-manifest.0b7ba8d65af44b59c308a0e31e6a40f3.js"
+  "/r34-react/precache-manifest.5a2b1f8d070796efdaa3793166523dcc.js"
 );
 
 workbox.clientsClaim();

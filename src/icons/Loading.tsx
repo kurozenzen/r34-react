57fdffb3648b5c78926dbce6a400ea83e235a297
kurozenzen <kurozenzen@gmail.com>
2021-03-02 01:12:19 +0100

    ⬆ Version bumped to 2.3.6

--- src/icons/Loading.tsx
+++ src/icons/Loading.tsx
@@ -10,7 +10,6 @@ const StyledSvg = styled.svg`
 export default function Loading() {
   return (
     <StyledSvg
-      version='1.1'
       xmlns='http://www.w3.org/2000/svg'
       xmlnsXlink='http://www.w3.org/1999/xlink'
       viewBox='0 0 50 20'

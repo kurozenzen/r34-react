30fc51ef714ca706c9e74d1202a448f545d4d158
kurozenzen <kurozenzen@gmail.com>
2021-08-10 11:19:12 +0200

    🧯 Fixed Reset buttons

--- src/components/widgets/ResetResultsButton.tsx
+++ src/components/widgets/ResetResultsButton.tsx
@@ -11,7 +11,7 @@ export default function ResetResultsButton() {
 
   return (
     <BlockButton onClick={handleClick}>
-      <UndoIcon /> Reset Application
+      <UndoIcon /> Reset Results
     </BlockButton>
   )
 }

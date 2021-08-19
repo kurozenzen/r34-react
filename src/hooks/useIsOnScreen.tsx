d4d6e30f182d995f02ab345f545c82f3515d1a05
kurozenzen <kurozenzen@gmail.com>
2021-08-19 10:34:11 +0200

    🎆 useIsOnScreennow shares the observer
    closes #20

--- src/hooks/useIsOnScreen.tsx
+++ src/hooks/useIsOnScreen.tsx
@@ -1,26 +1,44 @@
-import { useCallback, useEffect, useState } from 'react'
+import React from 'react'
 
-export default function useIsOnScreen(target: Element | null, options?: IntersectionObserverInit) {
-  const [isOnScreen, setOnScreen] = useState(false)
+// List of handlers paired with the target element for identification
+let handlers: Array<{ element: Element; update: (isOnScreen: boolean) => void }> = []
 
-  const handleOnScreenChange = useCallback((entries: IntersectionObserverEntry[]) => {
-    const [entry] = entries
-    setOnScreen(entry.isIntersecting)
+// global handler that can handle multiple entires at the same time
+const handleOnScreenChange = (entries: IntersectionObserverEntry[]) => {
+  entries.forEach((entry) => {
+    const handler = handlers.find((h) => h.element === entry.target)
+
+    if (handler) {
+      handler.update(entry.isIntersecting)
+    }
+  })
+}
+
+// single observer
+const observer = new IntersectionObserver(handleOnScreenChange, { threshold: 1 })
+
+/**
+ * Provides a boolean value indicating whether the {@link target} is fully visible on screen
+ */
+export default function useIsOnScreen(target: Element | null) {
+  const [isOnScreen, setOnScreen] = React.useState(false)
+
+  const onIsOnScreenChange = React.useCallback((value: boolean) => {
+    setOnScreen(value)
   }, [])
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(handleOnScreenChange, options || { threshold: 1, root: null })
-
+  React.useEffect(() => {
     if (target) {
+      const handler = { element: target, update: onIsOnScreenChange }
+      handlers.push(handler)
       observer.observe(target)
-    }
 
-    return () => {
-      if (target) {
+      return () => {
+        handlers.filter((h) => h !== handler)
         observer.unobserve(target)
       }
     }
-  }, [handleOnScreenChange, options, target])
+  }, [onIsOnScreenChange, target])
 
   return [isOnScreen]
 }

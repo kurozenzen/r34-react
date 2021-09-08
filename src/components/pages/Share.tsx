0aebe54d4e35ba35e3d016f1ce4d75f9b4ac71f2
kurozenzen <kurozenzen@gmail.com>
2021-09-08 14:09:48 +0200

    🔄 Updated dependency versions
    Added workaround for typescript#33923

--- src/components/pages/Share.tsx
+++ src/components/pages/Share.tsx
@@ -95,7 +95,7 @@ export default function About() {
   )
 
   const copySupertagUrl = React.useCallback(() => {
-    navigator.permissions.query({ name: 'clipboard-write' }).then((result) => {
+    navigator.permissions.query({ name: 'clipboard-write' as PermissionName }).then((result) => {
       if (result.state === 'granted' || result.state === 'prompt') {
         navigator.clipboard.writeText(window.location.href).then(
           () => {},

defa6b1cb34cc4c4013d19faa68c811fa2312315
kurozenzen <kurozenzen@gmail.com>
2021-09-14 12:26:27 +0200

    ⬇ Downgraded react-redux dependency

--- src/components/widgets/ScrollToTopButton.tsx
+++ src/components/widgets/ScrollToTopButton.tsx
@@ -1,14 +1,37 @@
 import React from 'react'
+import { useLocation } from 'react-router'
+import { Link } from 'react-router-dom'
+import { RouteName } from '../../data/types'
 
 import { ArrowUp } from '../../icons/FontAwesomeIcons'
 import { BlockButton } from '../designsystem/Buttons'
 
 export default function ScrollToTopButton() {
+  const location = useLocation()
+
+  if (location.pathname === RouteName.STORIES) {
+    return <NavigateBackButton />
+  } else {
+    return <ScrollUpButton />
+  }
+}
+
+function ScrollUpButton() {
   const scrollUp = React.useCallback(() => window.scrollTo(0, 0), [])
 
   return (
     <BlockButton onClick={scrollUp}>
       <ArrowUp /> Take me to the top
     </BlockButton>
+  )
+}
+
+function NavigateBackButton() {
+  return (
+    <Link to={RouteName.SEARCH}>
+      <BlockButton>
+        <ArrowUp /> Take me to the top
+      </BlockButton>
+    </Link>
   )
 }
